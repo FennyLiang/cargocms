90e836a8d28d7223aceb58756f39b6c9224d95d8
FennyLiang <shras1993@gmail.com>
2016-08-24 17:14:15 +0800

    use another way to  create user ui test

--- test/e2e/admin/user.js
+++ test/e2e/admin/user.js
@@ -18,20 +18,6 @@ describe.only('test user', () => {
         }
       });
 
-    it('Update user infomation', async(done) => {
-
-      try{
-
-        const userInfo = {
-          
-        }
-
-      }catch(e){
-        done(e);
-      }
-
-    })
-
 
     it('create @watch',async (done) => {
     try {
@@ -46,28 +32,66 @@ describe.only('test user', () => {
       browser.url('http://localhost:1338/admin/#/admin/user');
       browser.waitForExist('#ToolTables_main-table_1',2000)
       browser.click('#ToolTables_main-table_1');
-      browser.waitForExist('[class="btn btn-primary"]',1000);
+
+      const searchInput = browser.element('#content');
+
+      searchInput.waitForExist(1000);
+
       //填入資料
-      browser.setValue('[name="username"]', userData.username)
-      .setValue('[name="email"]', userData.email)
-      .setValue('[name="firstName"]', userData.firstName)
-      .setValue('[name="lastName"]', userData.lastName)
-      .setValue('[name="password"]', userData.password)
-      .setValue('[name="passwordConfirm"]', userData.password);
+      searchInput
+        .setValue('[name="username"]', userData.username)
+        .setValue('[name="email"]', userData.email)
+        .setValue('[name="firstName"]', userData.firstName)
+        .setValue('[name="lastName"]', userData.lastName)
+        .setValue('[name="password"]', userData.password)
+        .setValue('[name="passwordConfirm"]', userData.password);
+
       //送出
-      browser.click('[class="btn btn-primary"]')
-      .waitForExist('[class="btn btn-primary"]',1000,true);
+      browser
+        .click('#main-form footer button[type="submit"]')
+        .waitForExist('#main-table-widget', 1000, true);
+
+      //降冪排序
+      browser.waitForExist('#main-table-widget tr th:nth-child(1)', 1000);
+      browser.click('#main-table-widget tr th:nth-child(1)');
+
       //檢查
-      const res = await User.find({where: {username: userData.username}});
-      res.username.should.be.eq(userData.username);
-      res.email.should.be.eq(userData.email);
+      const res = User.find({where: {email: userData.email}});
+      const usernameField = browser.element('#main-table-widget tbody tr:nth-child(1) td:nth-child(4)');
+
+
+      expect(res.email).to.be.equal(usernameField);
 
       // expect(browser.elements('#ToolTables_main-table_1')!=null).to.equal(true);
-      done();
+      //done();
     } catch (e) {
       done(e);
     }
   });
+
+    it('Update user infomation', async(done) => {
+
+      try{
+        const updateUser = 'LALA';
+        const userInfo = {
+          username: 'BrooklynBackham',
+          email: 'brooklynBay@email.com',
+          firstName: 'Brooklyn',
+          lastName: 'Backham',
+          password: 'chloe'
+        }
+
+        browser.waitForExist('[type=search]');
+        browser.setValue('#main-table_filter input[type=search]', updateUser)
+        .click('#ToolTables_main-table_1')
+        .waitForExist('#main-form', 1000, true);
+
+        done();
+      }catch(e){
+        done(e);
+      }
+
+    })
 
     it.skip('user logout', (done) => {
 
